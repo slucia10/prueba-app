6b88d083e5548b99b6326069cb4d6482066507af
slucia10 <suarezlucia29@gmail.com>
2022-06-01 18:47:39 -0300

    cambios varios

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Bienvenidos a mi App
+        </h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
